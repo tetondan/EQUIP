be91c23bd95c94e4cc218cf9e7d4777af380e9f4
tetondan <dfrehner1@gmail.com>
2016-01-07 11:46:45 -0800

    updated Schema

--- app/server/models/message.js
+++ app/server/models/message.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 
 var messageSchema = mongoose.Schema({
   name: {type: String, required: true},
-  items: [{type: Schema.Types.ObjectId, ref: 'Item'}],
-  business: {type: Schema.Types.ObjectId, ref: 'Business'},
   email: {type: String},
-  phone: {type: String}
+  phone: {type: String},
+  dates: [{type: Number}],
+  items: [{type: Schema.Types.ObjectId, ref: 'Item'}]
 });
 
 var Message = mongoose.model('Message', messageSchema);
