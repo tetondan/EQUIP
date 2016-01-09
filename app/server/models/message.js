eccc5afbc99a09b25c46d575b3590424b8e60afc
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-09 14:10:46 -0800

    fix db schema for messages

--- app/server/models/message.js
+++ app/server/models/message.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
-var Business = require('./business'); 
-var Item = require('./item'); 
+var Business = require('./business');
+var Item = require('./item');
 
 var messageSchema = mongoose.Schema({
   name: {type: String, required: true},
   email: {type: String},
   phone: {type: String},
-  dates: [{type: Number}],
+  dates: [{type: Date}],
   items: [{type: mongoose.Schema.Types.ObjectId, ref: 'Item'}],
   businessId: {type: mongoose.Schema.Types.ObjectId, ref: 'Business'}
 });
