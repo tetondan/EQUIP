7e810df1c93bd6be17bf648a92b540790220a102
ZACK DEAN <vanleuvenze@gmail.com>
2016-01-09 20:57:46 -0800

    messy right now, added the inventory view as a nested state of the main page

--- app/client/Business/main/main.js
+++ app/client/Business/main/main.js
@@ -1,5 +1,10 @@
-angular.module('main.control', [])
-.controller('MainControl', function ($scope) {
-
-
-})
+angular.module('main.control', ['equip.services'])
+  .controller('MainControl', function ($scope, Inventory,$state) {
+    
+    //this will transition us into the inventory view
+    $state.transitionTo('main.inventory');
+  })
+  .config(function() {
+    // $mdIconProvider
+    //   .iconSet('communication', 'img/icons/sets/communication-icons.svg', 24);
+  })
