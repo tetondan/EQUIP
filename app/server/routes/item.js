bb01f2246ec279660acd66abef60f811e90029bd
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-10 10:20:14 -0800

    cleanup items route

--- app/server/routes/item.js
+++ app/server/routes/item.js
@@ -6,14 +6,16 @@ var router = express.Router();
 //gets ALL items in the database, not really good for anything but testing;
 router.route('/items').get(function (req, res) {
   Item.find(function(err, items){
-    if(err){console.log(err)};
-    res.status(200).send(items)
+    if(err){
+      console.log(err)
+    };
+    res.status(200).send(items);
   })
 });
 
 //creates a new item in the database, (item, price, amount, and business id are required);
 router.route('/items').post(function (req, res) {
-  var data = req.body
+  var data = req.body;
   var item = new Item({
     item: data.item,
     price: data.price,
@@ -28,50 +30,54 @@ router.route('/items').post(function (req, res) {
     if (err){
       console.log(err);
       res.status(404);
-    } else {  
-      res.status(201).send(item._id); 
+    } else {
+      res.status(201).send(item._id);
     }
-  })
+  });
 });
 
 //gets an item by the item id;
 router.route('/items/:id').get(function (req, res) {
   var itemId = req.params.id;
   Item.findById(itemId, function(err, item){
-    res.status(200).send(item)
-  })
+    res.status(200).send(item);
+  });
 });
 
 //allows the business owner to update items by the item id;
 router.route('/items/:id').put(function (req, res) {
-  var newItem = req.body
+  var newItem = req.body;
   Item.findById(req.params.id, function(err, item){
-    console.log(req.body)
     for(var key in req.body){
       item[key] = req.body[key];
     }
+    // TODO: doesn't this callback need an item
     item.save(function(err){
-      if(err){console.log(err)}
-      res.status(200).send(item)
-    })
-  })
+      if(err){
+        // TODO: need to resolve if can't save item
+        console.log(err);
+      } else {
+        res.status(200).send(item);
+      }
+    });
+  });
 });
 
 //allows the business owner to delete items from inventory;
 router.route('/items/:id').delete(function (req, res) {
   var itemId = req.params.id;
   Item.remove({'_id': itemId}, function(err, item){
-    res.status(200).send('removed')
-  })
+    res.status(200).send('removed');
+  });
 });
 
 //gets all items by buisness id (to be used for customer view page);
 router.route('/items/getall/:busid').get(function (req, res){
-  var busId = req.params.busid
+  var busId = req.params.busid;
   Item.find({businessId: busId}, function(err, items){
-    res.status(200).send(items)
-  })
-})
+    res.status(200).send(items);
+  });
+});
 
 module.exports = router;
 
@@ -80,7 +86,6 @@ module.exports = router;
 //   price: {type: Number, required: true},
 //   desc: {type: String},
 //   amt: {type: Number, required: true},
-//   isIn: {type: Boolean, required: true},
 //   img: {type: String},
 //   dates: [{type: Number}]
 // }
@@ -98,5 +103,3 @@ module.exports = router;
 // send it through our custom error handler
 // app.use(helpers.errorLogger);
 // app.use(helpers.errorHandler);
-
-
