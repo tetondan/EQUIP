d1cf51fdf8d4b33bdd4206e253aa96d4d0bc2203
ZACK DEAN <vanleuvenze@gmail.com>
2016-01-09 20:57:46 -0800

    messy right now, added the inventory view as a nested state of the main page

--- app/client/Business/appControl.js
+++ app/client/Business/appControl.js
@@ -2,10 +2,6 @@
 angular.module('equip.control', ['equip.services', 'auth.control', 'ngMaterial'])
   .controller('EquipControl', function ($scope, $location, Auth) {
   	
-    //fixture inventories
-    $scope.inventory = {
-      'bobsBoots': [{name: 'skis'}, {name: 'boots'}, {name: 'jacket'}]
-    };  
 
     //determine our route when this controller is loaded - this will display the sign in page if the user is not signe in, and the main page if 
     if (!Auth.isAuthorized()) {
