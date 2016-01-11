a2bf89b9b671f00e2d0bc32f3109a16d8d0f1eb9
ZACK DEAN <vanleuvenze@gmail.com>
2016-01-10 23:49:24 -0800

    main layout finished

--- app/client/Business/appControl.js
+++ app/client/Business/appControl.js
@@ -1,6 +1,6 @@
 // top level Equip controller
 angular.module('equip.control', ['equip.services', 'auth.control', 'ngMaterial', 'main.control'])
-  .controller('EquipControl', function ($scope, $location, Auth) {
+  .controller('EquipControl', function ($scope, $timeout,$log, $location, $mdSidenav, Auth) {
 
 
     //determine our route when this controller is loaded - this will display the sign in page if the user is not signe in, and the main page if 
@@ -9,5 +9,48 @@ angular.module('equip.control', ['equip.services', 'auth.control', 'ngMaterial',
     } else {
       $location.path('/main')
     }
+
+    $scope.toggleLeft = buildDelayedToggler('left');
+
+    $scope.isOpenRight = function(){
+      return $mdSidenav('right').isOpen();
+    };
+    /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait, context) {
+      var timer;
+      return function debounced() {
+        var context = $scope,
+            args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 10);
+      };
+    }
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    function buildDelayedToggler(navID) {
+      return debounce(function() {
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }, 200);
+    } 
    
+  })
+  .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function () {
+      $mdSidenav('left').close()
+        .then(function () {
+          $log.debug("close LEFT is done");
+        });
+    };
   })
--- app/client/Business/inventory/inventory.js
+++ app/client/Business/inventory/inventory.js
@@ -1,4 +1,4 @@
-angular.module('inventory.control', ['equip.services'])
+angular.module('inventory.control', ['equip.services', 'ngMaterial'])
   .controller('InventoryControl', function ($scope, Inventory) {
 
   	Inventory.getItems()
@@ -6,5 +6,23 @@ angular.module('inventory.control', ['equip.services'])
         $scope.inventory = data.data;
         console.log($scope.inventory);
       })
+
+    $scope.addTo = function () {
+      var itemData = {
+        item: $scope.item,
+        price: $scope.price,
+        desc: $scope.desc,
+        amt: $scope.amt,
+        isIn: $scope.isIn,
+        img: $scope.img,
+        dates: $scope.dates
+      }
+      console.log('HOWDY PARTNER')
+      Inventory.addItem(itemData)
+        .then(function (response) {
+          console.log('good POST', response);
+          return response;
+        })
+    }
     
   })
