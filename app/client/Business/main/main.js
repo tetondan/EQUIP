9ba45b254b1697d653c611ded42f413bdadc7c8a
ZACK DEAN <vanleuvenze@gmail.com>
2016-01-10 17:09:39 -0800

    added sidebar to main view, rendering messages now - they still need to be formatted however

--- app/client/Business/main/main.js
+++ app/client/Business/main/main.js
@@ -1,8 +1,15 @@
 angular.module('main.control', ['equip.services', 'ngMaterial'])
-  .controller('MainControl', function ($scope, Inventory,$state, $log, $timeout) {
+  .controller('MainControl', function ($scope, Messages, Inventory,$state, $log, $timeout) {
 
     //this will transition us into the inventory view
     $state.transitionTo('main.inventory');
+
+    //populate our messages sidebar
+    Messages.getMessages()
+      .then(function (messages) {
+        $scope.messages = messages.data;
+      });
+
   })
   .config(function() {
     // $mdIconProvider
