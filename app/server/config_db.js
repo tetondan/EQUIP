51caf9d1177257f9bd70f77e34527763a2c73bf4
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-11 09:31:35 -0800

    set up mongodb uri

--- app/server/config_db.js
+++ app/server/config_db.js
@@ -1,13 +1,21 @@
 var mongoose = require('mongoose');
 
 mongoURI = process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost/fearlessgerbil';
-mongoose.connect(mongoURI);
 
+// if want to force local mongo connection also set LOCATION to local by typing in
+// export LOCATION=LOCAL
+// then type in node server.js ...etc
+if (process.env.LOCATION === "LOCAL") {
+	mongoURI = 'mongodb://localhost/fearlessgerbil';
+}
+
+mongoose.connect(mongoURI);
 // Run in seperate terminal window using 'mongod'
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
  console.log('Mongodb connection open');
+ console.log(mongoURI);
 });
 
 module.exports = db;
